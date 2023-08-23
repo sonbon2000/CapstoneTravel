0b8f43a0021230a0465429e212fb3f38fc9690f9
son <sonbon1101@gmail.com>
2023-08-23 20:58:52 +0700

    fix README

---
+++ src/client/test/formHandler.text.js
@@ -0,0 +1,7 @@
+import { onSubmit } from "../js/formDataHandler";
+
+describe("Check onSubmit function", () => {
+  test("Check the function defined", () => {
+    expect(onSubmit).toBeDefined();
+  });
+});
